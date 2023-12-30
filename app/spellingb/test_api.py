f15ddd71e0b4a536ba6c6e74dd5da8a947d45fde
Peter Lukacs <lukacs.peter.andras@gmail.com>
2023-12-30 13:55:00 +0100

    update api tester script

--- app/spellingb/test_api.py
+++ app/spellingb/test_api.py
@@ -1,7 +1,8 @@
 import requests
 
 if __name__ == "__main__":
-    URL = "http://spelli-publi-ijlxfutr2ajd-1656619316.eu-west-1.elb.amazonaws.com/api/puzzle"
+    DEV_URL = "https://spelli-publi-ijlxfutr2ajd-1656619316.eu-west-1.elb.amazonaws.com/api/puzzle"
+    PROD_URL = "https://spelli-Publi-bOtR1axg8afS-1668478668.eu-west-1.elb.amazonaws.com/api/puzzle"
     # for i in range(100):
     #     print(f"Request #{i + 1}")
     #     response = requests.post(
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     #     print(f"Response code: {response.status_code}")
 
     response = requests.post(
-        url=URL,
+        url=DEV_URL,
         json={"letters": "", "main_letter": ""},
     )
     print(response.json())
